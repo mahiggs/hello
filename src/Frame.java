9cd5a7c0528f25358bdbe20044a34f1d5fe60b54
Alwin224 <alwinbhogal2@gmail.com>
2023-03-02 00:24:05 -0600

    Added things

--- src/Frame.java
+++ src/Frame.java
@@ -1,21 +1,40 @@
+
+import java.awt.BorderLayout;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 
 public class Frame extends JFrame{
 
-	
+
 	protected static final int X_Location = 500;
 	protected static final int Y_Location = 150;
-	
-	
+	protected JLabel label;
+	protected JFrame frame;
+	protected ImageIcon image;
+
 	public Frame() {
-		
-		
-		JFrame frame = new JFrame();
-		
+
+
+		frame = new JFrame();
+
+
+
 		frame.setSize(500, 500);
 		frame.setLocation(X_Location, Y_Location);
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		frame.setTitle("Dr.Higgs Frame");
+
+		label = new JLabel();
+		label.setText("This is Dr.Higgs");
+		label.setIcon(image);
+		label.setHorizontalTextPosition(JLabel.CENTER);
+		
+		frame.add(label);
+
 		frame.setVisible(true);
 	}
 }
